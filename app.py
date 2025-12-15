6dc3284a5c8c082e6f04777bffd4358889c22e90
Charles Gahide <charles.gahide@student.ucll.be>
2025-12-15 16:54:42 +0100

    basic movememnt

--- app.py
+++ app.py
@@ -9,18 +9,22 @@ pygame.init()
 class State:
     def __init__(self):
         self.x_circle = 0
+        self.y_circle = 0
 
-    def update_pos(self, x):
+    def update_pos_x(self, x):
         self.x_circle+=x
+
+    def update_pos_y(self, y):
+        self.y_circle +=y
+        
 
     def render_frame(self, surface):
         pygame.draw.rect(surface, (0,0,0,),(0,0,1024,768),)
-        pygame.draw.circle(surface, (155, 111, 111), (self.x_circle, 200), 50)
+        pygame.draw.circle(surface, (155, 111, 111), (self.x_circle, self.y_circle), 50)
         flip()
 
 
 # Tuple representing width and height in pixels
-
 def create_main_surface():
     screen_size = (1024, 768)
     # Create window with given size
@@ -29,6 +33,7 @@ def create_main_surface():
 def main():
     pos_state=State()
     pos_state.x_circle = 500
+    pos_state.y_circle = 500
     
     clock = pygame.time.Clock()
     speed = 200   # pixles per seconde
@@ -39,12 +44,23 @@ def main():
         dt = clock.tick(60)/1000 #seconds per frame
 
         pos_state.render_frame(surface)
-        pos_state.update_pos(speed * dt)
+        #
+        
+
         
         # Basic event handling to keep the window responsive (best practice)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+
+        # Check pressed keys outside the event loop
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:          # ⬆️ Up arrow key
+            pos_state.update_pos_y(-5)
+        if keys[pygame.K_DOWN]:        # ⬇️ Down arrow key
+            pos_state.update_pos_y(5)
+                
 
 if __name__ == "__main__":
     main()
