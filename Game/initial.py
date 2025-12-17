ee897983a1e8bfa6c55550b72132caea94e61ca2
Sander Eggers <r0856120@ucll.be>
2025-12-17 15:24:58 +0100

    enemy changes

--- Game/initial.py
+++ Game/initial.py
@@ -23,7 +23,7 @@ def main():
     camera = Camera(level_module.LEVEL_WIDTH, level_module.LEVEL_HEIGHT)
     player = Player(lvl, camera) # Player now takes level and camera
     # Initialize enemy at a safe spot, e.g., 800, 400 or somewhere valid
-    y_enemy = random.randint(1,LEVEL_HEIGHT - 50)
+    y_enemy = random.randint(100,500)
     enemy = Enemy(800, y_enemy) 
     running = True
     running = True
